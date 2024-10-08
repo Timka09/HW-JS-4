c22837f415a9fd836a6b2fbed177fed093f433b2
Timka09 <tymofij.i.naumenko@gmail.com>
2024-10-08 20:50:35 +0300

    зфз

---
+++ hw.js
@@ -0,0 +1,7 @@
+// Task 3
+const inputik = prompt("Введіть текст");
+if (inputik.includes("JavaScript")) {
+  alert("Текст містить слово JavaScript");
+} else {
+    alert("Текст не містить слово JavaScript");
+}
